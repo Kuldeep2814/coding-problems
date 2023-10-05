19740d23247354f7225b12d96f0c46b6ba64d6b0
Kuldep2814 <kumar.kuldeep2814@gmail.com>
2023-10-05 23:26:30 +0530

    Problem8

---
+++ PhoneNumber.js
@@ -0,0 +1,35 @@
+function getPhoneNumber(input) {
+    // Write your code here
+    const getPhoneMap = {
+        zero: '0',
+        one: '1',
+        two: '2',
+        three: '3',
+        four: '4',
+        five: '5',
+        six: '6',
+        seven: '7',
+        eight: '8',
+        nine: '9',
+    };
+    
+    const words = input.split(' ');
+    let result = '';
+    
+    for (const word of words){
+        
+        if (word === 'double' || words === 'triple'){
+            const nextWord = words[words.indexOf(word) + 1];
+            const digit = getPhoneMap[nextWord[0]];
+            const repeatCount = word === 'double' ? 2 : 3;
+            result += digit.repeat(repeatCount);
+        } else {
+            result += getPhoneMap[word];
+        }
+    }
+    
+    return result;
+}
+
+
+console.log(getPhoneNumber("two one nine six eight one six four six zero"));
