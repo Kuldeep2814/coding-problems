2e6d94b5820aa2f2e175e587f8099bddf0a09d3b
Kuldep2814 <kumar.kuldeep2814@gmail.com>
2023-10-11 22:53:21 +0530

    DSA PROBLEM2

---
+++ DSA/ArrayValidation.js
@@ -0,0 +1,12 @@
+let data = [2, 89, 46, 57, 64];
+
+function getElement() {
+    let element = 3;
+    if(element<data.length) {
+        console.log(data[element])
+    }else {
+        console.log("Element is not found")
+    }
+}
+
+getElement();
